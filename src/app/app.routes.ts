5da14d0ad5c4048b68e4688ef9b76bb239361780
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-07-17 14:06:03 +0200

    feat: Implement the basic project structure.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'items',
+    loadComponent: () => import('./items/items.component').then(m => m.ItemsComponent)
+  }
+];
---
+++ src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,39 @@
+import {Component} from '@angular/core';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatFabButton, MatIconButton} from "@angular/material/button";
+import {MatListItem, MatNavList} from "@angular/material/list";
+import {RouterLink, RouterOutlet} from "@angular/router";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
+import {MatSlideToggle} from "@angular/material/slide-toggle";
+import {NgStyle, TitleCasePipe} from "@angular/common";
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [
+    MatSidenavContent,
+    MatToolbar,
+    MatIconButton,
+    MatSidenavContainer,
+    MatSidenav,
+    MatNavList,
+    MatListItem,
+    RouterLink,
+    MatIconModule,
+    RouterOutlet,
+    MatMenuTrigger,
+    MatMenu,
+    MatMenuItem,
+    MatSlideToggle,
+    NgStyle,
+    MatFabButton,
+    TitleCasePipe,
+  ],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss',
+
+})
+export class ToolbarComponent {
+}
