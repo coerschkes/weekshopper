72c30ed9b48276cb0bbebf1d74870042ad79316c
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-07-31 13:51:00 +0200

    feat: Add the badges and badge selector to the edit bottom sheet.

--- src/app/items/edit-item/edit-item.component.ts
+++ src/app/items/edit-item/edit-item.component.ts
@@ -5,6 +5,9 @@ import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatIconModule} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
 import {EditItemService} from "./edit-item.service";
+import {AutocompleteSelector, Filterable} from "../../shared/autocomplete-selector/autocomplete-selector.component";
+import {BadgeComponent} from "../../shared/badge/badge.component";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-edit-item',
@@ -14,7 +17,9 @@ import {EditItemService} from "./edit-item.service";
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    AutocompleteSelector,
+    BadgeComponent
   ],
   templateUrl: './edit-item.component.html',
   styleUrl: './edit-item.component.scss'
@@ -23,4 +28,7 @@ export class EditItemComponent {
   constructor(protected editItemService: EditItemService) {
   }
 
+  get dataset(): Filterable[] {
+    return environment.INITIAL_CATEGORIES;
+  }
 }
--- src/app/items/edit-item/edit-item.service.ts
+++ src/app/items/edit-item/edit-item.service.ts
@@ -1,16 +1,37 @@
 import {Injectable, Signal, signal, WritableSignal} from "@angular/core";
 import {Item} from "../../core/domain/item";
+import {BadgeService} from "../../shared/badge/badge.service";
+import {Category} from "../../core/domain/category";
+import {Badge} from "../../shared/badge/badge.component";
 
 @Injectable({providedIn: 'root'})
 export class EditItemService {
   private _item: WritableSignal<Item | undefined> = signal(undefined);
 
+  constructor(private _badgeService: BadgeService) {
+  }
+
   updateItem(item: Item) {
     this._item.update(() => item)
+    this._badgeService.reset()
+    this._item()?.category.forEach(category => this._badgeService.addBadge({name: category.name, icon: category.icon}))
   }
 
   get item(): Signal<Item | undefined> {
     return this._item.asReadonly();
   }
 
+  removeBadge(badge: Badge) {
+    this._badgeService.removeBadge(badge)
+    this._item.update(item => {
+      item!.category = item!.category.filter(category => category.name !== badge.name);
+      return item
+    })
+  }
+
+  addCategory(category: Category) {
+    console.log(category)
+    this.item()?.category.push(category)
+    this._badgeService.addBadge({name: category.name, icon: category.icon})
+  }
 }
