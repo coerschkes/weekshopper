0bc2af2b0c9f2f0daed14fd1124046c676995666
Christian Oerschkes <christian.oerschkes@hotmail.de>
2024-07-31 13:45:57 +0200

    feat: Initialize categories in environments for now.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,11 @@
 export const environment = {
   APP_NAME: 'Weekshopper',
+  INITIAL_CATEGORIES: [
+    {id: 1, name: "Vegetables", icon: "cabbage.png"},
+    {id: 2, name: "Canned food", icon: "canned-food.png"},
+    {id: 3, name: "Frozen food", icon: "frozen-goods.png"},
+    {id: 4, name: "Noodles", icon: "noodles.png"},
+    {id: 5, name: "Drugstore", icon: "drugstore.png"},
+    {id: 6, name: "Other", icon: "delivery-box.png"}
+  ]
 };
